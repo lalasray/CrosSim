a3f1fcc443ccd180c600688e505e5d8a613804f1
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2024-12-23 01:50:35 +0100

    update

--- Model/crossim_pretrain.py
+++ Model/crossim_pretrain.py
@@ -1,30 +1,45 @@
-import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from random_dataloader import RandomCrosSimDataset, collate_fn
-
-# Parameters
-num_samples = 30  # Number of samples in the dataset
-num_positions = 20  # Number of IMU positions
-imu_shape = (1200, 3)  # Shape of each IMU sensor's accelerometer and gyroscope data
-embedding_dim = 768  # Dimension of the embedding vector
-pose_shape = (300, 22, 3)  # Shape of the pose data (frames, joints, coordinates)
-num_classes = 3000  # Number of classes for the one-hot embedding
-
-# Create the random dataset and dataloader
-random_dataset = RandomCrosSimDataset(num_samples, num_positions, imu_shape, embedding_dim, pose_shape, num_classes)
-random_dataloader = DataLoader(random_dataset, batch_size=16, shuffle=True, collate_fn=collate_fn)
+from class_encoder import ClassEncoder
+from clip_text_encoder import TextEncoder
+from graph_IMU_encoder import IMUGraphEncoderTemporal
+from graph_pose_encoder import PoseGraphEncoderTemporal
+from gtr_text_encoder import EmbeddingEncoder
+from Single_IMU_with_pos_encoder import IMUSingleNodeEncoderWithClass
 
 # Example usage
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-for npy_data, text_data, embedding_data, imu_data, one_hot_embedding, npy_paths in random_dataloader:
-    print(f"Pose shape: {npy_data.shape}")
-    print(f"Embedding shape: {embedding_data.shape}")
-    print(f"One-hot embedding shape: {one_hot_embedding.shape}")
-    print(f"Text data: {text_data}")
-    print(f"IMU positions: {len(imu_data.keys())}")
-    for position, data in imu_data.items():
-        print(f"Position: {position}, Acc shape: {data['acc'].shape}, Gyr shape: {data['gyr'].shape}")
-    break  # Print only one batch for demonstration
+# Parameters
+num_samples = 30  # Number of samples in the dataset
+num_positions = 20  # Number of IMU positions
+imu_shape = (1200, 3)  # Shape of each IMU sensor's accelerometer and gyroscope data
+text_embedding_dim = 768  # Dimension of the embedding vector
+pose_shape = (300, 22, 3)  # Shape of the pose data (frames, joints, coordinates)
+num_classes = 300  # Example: 10 classes
+batch_size = 16
+embedding_dim = 512  # Size of the embedding space
+imu_nodes = 20
+imu_dim = 6
+pose_joints = 22
+
+# Create the random dataset and dataloader
+random_dataset = RandomCrosSimDataset(num_samples, num_positions, imu_shape, text_embedding_dim, pose_shape, num_classes)
+random_dataloader = DataLoader(random_dataset, batch_size=16, shuffle=True, collate_fn=collate_fn)
+
+#activity label
+class_enc = ClassEncoder(num_classes, embedding_dim)
+clip_enc = TextEncoder()
+gtr_enc = EmbeddingEncoder()
+
+#imu data
+imu_enc = IMUGraphEncoderTemporal(num_nodes=imu_nodes, feature_dim=imu_dim, embedding_size=embedding_dim, max_hop=1, dilation=1, temporal_hidden_size=256)
+single_imu_enc = IMUSingleNodeEncoderWithClass(feature_dim=imu_dim, embedding_size=embedding_dim, temporal_hidden_size=256, num_classes=num_positions)
+
+#pose data
+pose_enc = PoseGraphEncoderTemporal(num_nodes=pose_joints, feature_dim=imu_dim, embedding_size=embedding_dim, max_hop=1, dilation=1, temporal_hidden_size=256)
+
+
+
