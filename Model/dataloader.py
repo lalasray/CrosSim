830b0f772122184d590297e0ed14be7ea5e1ed80
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2025-02-10 12:58:13 +0100

    Update dataloader.py

--- Model/dataloader.py
+++ Model/dataloader.py
@@ -73,22 +73,54 @@ class DancePoseDataset(Dataset):
 # Custom collate function to handle variable-length sequences
 def collate_fn(batch):
     # Simply return the batch as a list of tensors
-    motion_data, pose_data, imu_back_data = zip(*batch)
-    return list(motion_data), list(pose_data), list(imu_back_data)
+    motion_data, pose_data, imu_back_data, imu_belt_data, imu_chest_data, imu_forehead_data, imu_left_arm_data, imu_left_ear_data, imu_left_foot_data, imu_left_shin_data, imu_left_pocket_data, imu_left_shoulder_data, imu_left_thigh_data, imu_left_wrist_data, imu_necklace_data, imu_right_arm_data, imu_right_ear_data, imu_right_foot_data, imu_right_shin_data, imu_right_pocket_data, imu_right_shoulder_data, imu_right_thigh_data, imu_right_wrist_data = zip(*batch)
+
+    return list(motion_data), list(pose_data), list(imu_back_data), list(imu_belt_data), list(imu_chest_data), list(imu_forehead_data), list(imu_left_arm_data), list(imu_left_ear_data), list(imu_left_foot_data), list(imu_left_shin_data), list(imu_left_pocket_data), list(imu_left_shoulder_data), list(imu_left_thigh_data), list(imu_left_wrist_data), list(imu_necklace_data), list(imu_right_arm_data), list(imu_right_ear_data), list(imu_right_foot_data), list(imu_right_shin_data), list(imu_right_pocket_data), list(imu_right_shoulder_data), list(imu_right_thigh_data), list(imu_right_wrist_data)
+
+
 
 # Example usage
 root_dir = r"/media/lala/Seagate/CrosSim"
 pose_dir = r"/media/lala/Seagate/CrosSim"
 imu_dir = r"/media/lala/Seagate/CrosSim"
+target_dir = r"/media/lala/Seagate/temp"
+
 dataset = DancePoseDataset(root_dir, pose_dir, imu_dir)
 
-dataloader = DataLoader(dataset, batch_size=8, collate_fn=collate_fn, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=1, collate_fn=collate_fn, shuffle=False)
 
-for motion_data, pose_data, imu_back_data, imu_belt_data, imu_chest_data, imu_forehead_data, imu_left_arm_data, imu_left_ear_data, imu_left_foot_data, imu_left_shin_data, imu_left_pocket_data, imu_left_shoulder_data,imu_left_thigh_data, imu_left_wrist_data, imu_necklace_data, imu_right_arm_data, imu_right_ear_data, imu_right_foot_data, imu_right_shin_data, imu_right_pocket_data, imu_right_shoulder_data, imu_right_thigh_data, imu_right_wrist_data in dataloader:
+for motion_data, pose_data, imu_back_data, imu_belt_data, imu_chest_data, imu_forehead_data, imu_left_arm_data, imu_left_ear_data, imu_left_foot_data, imu_left_shin_data, imu_left_pocket_data, imu_left_shoulder_data, imu_left_thigh_data, imu_left_wrist_data, imu_necklace_data, imu_right_arm_data, imu_right_ear_data, imu_right_foot_data, imu_right_shin_data, imu_right_pocket_data, imu_right_shoulder_data, imu_right_thigh_data, imu_right_wrist_data in dataloader:
     print(f"Motion data shape (first sample): {motion_data[0].shape}")
     print(f"Pose data shape (first sample): {pose_data[0].shape}")
-    imu_back = imu_back_data[0]
-    print(f"IMU back gyro: {imu_back['angular_velocity'].shape}")
-    print(f"IMU back acc with grav: {imu_back['linear_acceleration_with_gravity'].shape}")
-    print(f"IMU back acc: {imu_back['linear_acceleration'].shape}")
+
+    imu_data = {
+        'back': imu_back_data,
+        'belt': imu_belt_data,
+        'chest': imu_chest_data,
+        'forehead': imu_forehead_data,
+        'left_arm': imu_left_arm_data,
+        'left_ear': imu_left_ear_data,
+        'left_foot': imu_left_foot_data,
+        'left_shin': imu_left_shin_data,
+        'left_pocket': imu_left_pocket_data,
+        'left_shoulder': imu_left_shoulder_data,
+        'left_thigh': imu_left_thigh_data,
+        'left_wrist': imu_left_wrist_data,
+        'necklace': imu_necklace_data,
+        'right_arm': imu_right_arm_data,
+        'right_ear': imu_right_ear_data,
+        'right_foot': imu_right_foot_data,
+        'right_shin': imu_right_shin_data,
+        'right_pocket': imu_right_pocket_data,
+        'right_shoulder': imu_right_shoulder_data,
+        'right_thigh': imu_right_thigh_data,
+        'right_wrist': imu_right_wrist_data,
+    }
+    
+    for sensor_name, sensor_data in imu_data.items():
+        print(f"IMU {sensor_name} gyro: {sensor_data[0]['angular_velocity'].shape}")
+        print(f"IMU {sensor_name} acc with grav: {sensor_data[0]['linear_acceleration_with_gravity'].shape}")
+        print(f"IMU {sensor_name} acc: {sensor_data[0]['linear_acceleration'].shape}")
+    
     break
+
