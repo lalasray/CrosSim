c7e4d35e8d70189d51df5245208433ce7e59adcb
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2025-03-10 16:03:29 +0100

    update

--- Model/Cluster_Pretraining_11.py
+++ Model/Cluster_Pretraining_11.py
@@ -1,22 +1,29 @@
-from Pretraining_JLR_11 import train_model
+import argparse
+
+# Define command-line argument parser
+parser = argparse.ArgumentParser(description="Run specific training model based on input command.")
+parser.add_argument("--cmt", type=str, required=True, help="Specify which training model to run")
+
+args = parser.parse_args()
+cmt = args.cmt  # Get the argument value
+
 h5_file_path = "../../../../ds/other/CrosSim/UniMocap/full_dataset.h5"
 batch_size = 1024
-train_model(epochs=300, batch_size=batch_size,h5_file_path = h5_file_path)
 
-from Pretraining_JLR_ft_11 import train_model_ft
-train_model_ft(epochs=300, batch_size=batch_size,h5_file_path = h5_file_path)
+# Dictionary mapping commands to functions
+train_functions = {
+    "train_model": lambda: __import__("Pretraining_JLR_11").train_model(epochs=300, batch_size=batch_size, h5_file_path=h5_file_path),
+    "train_model_ft": lambda: __import__("Pretraining_JLR_ft_11").train_model_ft(epochs=300, batch_size=batch_size, h5_file_path=h5_file_path),
+    "train_bimodel_ft": lambda: __import__("Pretraining_bimodal_textimu_ft_11").train_bimodel_ft(epochs=300, batch_size=batch_size, h5_file_path=h5_file_path),
+    "train_bimodel": lambda: __import__("Pretraining_bimodal_textimu_11").train_bimodel(epochs=300, batch_size=batch_size, h5_file_path=h5_file_path),
+    "train_bipose": lambda: __import__("Pretraining_bimodal_imupose_11").train_bipose(epochs=300, batch_size=batch_size, h5_file_path=h5_file_path),
+    "train_model_imubi": lambda: __import__("Pretraining_JLR_imubi_11").train_model_imubi(epochs=300, batch_size=batch_size, h5_file_path=h5_file_path),
+    "train_model_imubi_ft": lambda: __import__("Pretraining_JLR_imubi_ft_11").train_model_imubi_ft(epochs=300, batch_size=batch_size, h5_file_path=h5_file_path),
+}
 
-from Pretraining_bimodal_textimu_ft_11 import train_bimodel_ft
-train_bimodel_ft(epochs=300, batch_size=batch_size,h5_file_path = h5_file_path)
-
-from Pretraining_bimodal_textimu_11 import train_bimodel
-train_bimodel(epochs=300, batch_size=batch_size,h5_file_path = h5_file_path)
-
-from Pretraining_bimodal_imupose_11 import train_bipose
-train_bipose(epochs=300, batch_size=batch_size,h5_file_path = h5_file_path)
-
-from Pretraining_JLR_imubi_11 import train_model_imubi
-train_model_imubi(epochs=300, batch_size=batch_size,h5_file_path = h5_file_path)
-
-from Pretraining_JLR_imubi_ft_11 import train_model_imubi_ft
-train_model_imubi_ft(epochs=300, batch_size=batch_size,h5_file_path = h5_file_path)
+# Run the selected function if it exists
+if cmt in train_functions:
+    print(f"Running {cmt}...")
+    train_functions[cmt]()
+else:
+    print(f"Invalid cmt command: {cmt}")
--- Model/run.sh
+++ Model/run.sh
@@ -1,13 +1,8 @@
-srun -K  --job-name=lali \
-	 --partition=A100-80GB,H100,H200,RTXA6000 \
-	 --time=4-00:00 \
-	 --gpus=1 \
-	 --cpus-per-task=8 \
-	 --mem=200G \
-	 --output output/console.%A_%a.out \
-	 --error output/console.%A_%a.error  \
-         --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_23.07-py3.sqsh \
-         --container-workdir=`pwd` \
-         --container-mounts=/netscratch:/netscratch,/ds:/ds,`pwd`:`pwd` \
-         --export="NCCL_SOCKET_IFNAME=bond,NCCL_IB_HCA=mlx5" \
-          install.sh python Cluster_Pretraining_JLR_11.py "$@" &
+./run_1.sh train_model
+./run_1.sh train_model_ft
+./run_1.sh train_bimodel
+./run_1.sh train_bimodel_ft
+./run_1.sh train_bipose
+./run_1.sh train_model_imubi    
+./run_1.sh train_model_imubi_ft
+
