92f176cbc0ab4c5feb8d3eb7c9d767855b5686cc
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2025-03-11 17:27:24 +0100

    Update run.sh

--- Model/run.sh
+++ Model/run.sh
@@ -1,5 +1,8 @@
-./prepare.sh train_model
-./prepare.sh train_bimodel
-./prepare.sh train_bipose
-./prepare.sh train_model_imubi    
+./prepare.sh "train_multi"
+./prepare.sh "train_bitext"
+./prepare.sh "train_bipose"
+./prepare.sh "train_multi_imubi"
+./prepare.sh "train_bitext_down"
+./prepare.sh "train_bipose_down"
+  
 
